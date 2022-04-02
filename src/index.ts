9764e829cbd21f5e991194b8e1bc0a42cace9596
Quentin Lintz <quentinlintz@outlook.com>
2022-04-01 20:23:02 -0400

    Add starter metadata

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
 import { RecipeBuilder } from '@blitzjs/installer'
 
-export default RecipeBuilder().build()
+export default RecipeBuilder()
+  .setName('Mantine')
+  .setDescription(
+    'This will install all necessary dependencies and configure Mantine for use.'
+  )
+  .setOwner('Quentin Lintz <quentinlintz@outlook.com>')
+  .setRepoLink('https://github.com/quentinlintz/blitzjs-mantine-recipe')
+  .build()
