5431e288c40a9f87f32d35ab68dcdf8970b02a58
Quentin Lintz <quentinlintz@outlook.com>
2022-04-01 21:58:21 -0400

    Add getInitialProps to MyDocument

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,32 @@ import { addImport, paths, Program, RecipeBuilder } from '@blitzjs/installer'
 import type { NodePath } from 'ast-types/lib/node-path'
 import j from 'jscodeshift'
 
+function createGetInitialProps(program: Program) {
+  program.find(j.ExportDefaultDeclaration).forEach(path => {
+    path.insertBefore(
+      j.variableDeclaration('const', [
+        j.variableDeclarator(
+          j.identifier('getInitialProps'),
+          j.callExpression(j.identifier('createGetInitialProps'), [])
+        )
+      ])
+    )
+  })
+
+  program
+    .find(j.JSXElement)
+    .filter(
+      path =>
+        path.parent?.parent?.parent?.value?.id?.name === 'MyDocument' &&
+        path.parent?.value.type === j.ReturnStatement.toString()
+    )
+    .forEach(path => {
+      path.insertAfter('static getInitialProps = getInitialProps;')
+    })
+
+  return program
+}
+
 function wrapComponentWithMantineProvider(program: Program) {
   program
     .find(j.JSXElement)
@@ -44,8 +70,25 @@ export default RecipeBuilder()
     explanation: `Mantine has many packages, but core and hooks are the basics`,
     packages: [
       { name: '@mantine/core', version: 'latest' },
-      { name: '@mantine/hooks', version: 'latest' }
+      { name: '@mantine/hooks', version: 'latest' },
+      { name: '@mantine/next', version: 'latest' }
     ]
+  })
+  .addTransformFilesStep({
+    stepId: 'importMantineProvider',
+    stepName: 'Import MantineProvider component',
+    explanation: `Import the Mantine provider into _app, so it is accessible in the whole app`,
+    singleFileSearch: paths.document(),
+    transform(program) {
+      const stylesImport = j.importDeclaration(
+        [j.importSpecifier(j.identifier('createGetInitialProps'))],
+        j.literal('@mantine/next')
+      )
+
+      addImport(program, stylesImport)
+
+      return createGetInitialProps(program)
+    }
   })
   .addTransformFilesStep({
     stepId: 'importMantineProvider',
