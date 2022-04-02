222be0f69dd8e9583fe50730ad60395a10a77648
Quentin Lintz <98997219+quentinlintz@users.noreply.github.com>
2022-04-02 12:45:07 -0400

    Add steps (#2)
    * Add core, hooks, and next dependencies
    
    * Import MantineProvider
    
    * Fix MantineProvider props
    
    * Add getInitialProps to MyDocument
    
    * Remove getInitialProps

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,35 @@
-import { RecipeBuilder } from '@blitzjs/installer'
+import { addImport, paths, Program, RecipeBuilder } from '@blitzjs/installer'
+import type { NodePath } from 'ast-types/lib/node-path'
+import j from 'jscodeshift'
+
+function wrapComponentWithMantineProvider(program: Program) {
+  program
+    .find(j.JSXElement)
+    .filter(
+      path =>
+        path.parent?.parent?.parent?.value?.id?.name === 'App' &&
+        path.parent?.value.type === j.ReturnStatement.toString()
+    )
+    .forEach((path: NodePath) => {
+      const { node } = path
+      path.replace(
+        j.jsxElement(
+          j.jsxOpeningElement(j.jsxIdentifier(`MantineProvider`), [
+            j.jsxAttribute(j.jsxIdentifier('withGlobalStyles')),
+            j.jsxAttribute(j.jsxIdentifier('withNormalizeCSS')),
+            j.jsxAttribute(
+              j.jsxIdentifier('theme'),
+              j.jsxExpressionContainer(j.identifier("{ colorScheme: 'light' }"))
+            )
+          ]),
+          j.jsxClosingElement(j.jsxIdentifier('MantineProvider')),
+          [j.jsxText('\n'), node, j.jsxText('\n')]
+        )
+      )
+    })
+
+  return program
+}
 
 export default RecipeBuilder()
   .setName('Mantine')
@@ -7,4 +38,30 @@ export default RecipeBuilder()
   )
   .setOwner('Quentin Lintz <quentinlintz@outlook.com>')
   .setRepoLink('https://github.com/quentinlintz/blitzjs-mantine-recipe')
+  .addAddDependenciesStep({
+    stepId: 'addDeps',
+    stepName: 'Add npm dependencies',
+    explanation: `Mantine has many packages, but core and hooks are the basics`,
+    packages: [
+      { name: '@mantine/core', version: 'latest' },
+      { name: '@mantine/hooks', version: 'latest' },
+      { name: '@mantine/next', version: 'latest' }
+    ]
+  })
+  .addTransformFilesStep({
+    stepId: 'importMantineProvider',
+    stepName: 'Import MantineProvider component',
+    explanation: `Import the Mantine provider into _app, so it is accessible in the whole app`,
+    singleFileSearch: paths.app(),
+    transform(program) {
+      const stylesImport = j.importDeclaration(
+        [j.importSpecifier(j.identifier('MantineProvider'))],
+        j.literal('@mantine/core')
+      )
+
+      addImport(program, stylesImport)
+
+      return wrapComponentWithMantineProvider(program)
+    }
+  })
   .build()
