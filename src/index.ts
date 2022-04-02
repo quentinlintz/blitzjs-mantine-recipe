92e936b6400084f989c2d9b759f94219523748c5
Quentin Lintz <quentinlintz@outlook.com>
2022-04-01 20:56:27 -0400

    Import MantineProvider

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,29 @@
-import { RecipeBuilder } from '@blitzjs/installer'
+import { addImport, paths, Program, RecipeBuilder } from '@blitzjs/installer'
+import type { NodePath } from 'ast-types/lib/node-path'
+import j from 'jscodeshift'
+
+// Copied from https://github.com/blitz-js/blitz/pull/805, let's add this to the @blitzjs/installer
+function wrapComponentWithMantineProvider(program: Program) {
+  program
+    .find(j.JSXElement)
+    .filter(
+      path =>
+        path.parent?.parent?.parent?.value?.id?.name === 'App' &&
+        path.parent?.value.type === j.ReturnStatement.toString()
+    )
+    .forEach((path: NodePath) => {
+      const { node } = path
+      path.replace(
+        j.jsxElement(
+          j.jsxOpeningElement(j.jsxIdentifier('MantineProvider')),
+          j.jsxClosingElement(j.jsxIdentifier('MantineProvider')),
+          [j.jsxText('\n'), node, j.jsxText('\n')]
+        )
+      )
+    })
+
+  return program
+}
 
 export default RecipeBuilder()
   .setName('Mantine')
@@ -16,5 +41,21 @@ export default RecipeBuilder()
       { name: '@mantine/hooks', version: 'latest' },
       { name: '@mantine/next', version: 'latest' }
     ]
+  })
+  .addTransformFilesStep({
+    stepId: 'importProviderAndReset',
+    stepName: 'Import MantineProvider component',
+    explanation: `Import the Mantine provider into _app, so it is accessible in the whole app`,
+    singleFileSearch: paths.app(),
+    transform(program) {
+      const stylesImport = j.importDeclaration(
+        [j.importSpecifier(j.identifier('MantineProvider'))],
+        j.literal('@mantine/core')
+      )
+
+      addImport(program, stylesImport)
+
+      return wrapComponentWithMantineProvider(program)
+    }
   })
   .build()
